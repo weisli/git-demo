75cdbfdbff07caf4355f5696fb1d6ff774214122
shen <2867702414s@gmail.com>
2023-02-24 09:36:16 +0800

    23-2-24 hot-fix

--- forpython.py
+++ forpython.py
@@ -1,4 +1,7 @@
 num_years = ['1992','1993','1994']
 for num_year in num_years:
     print(num_year)
+print("Think you master!")
 print("Thank you hot-fix!")
+print("There is not your birthplace.")
+print("My birthplace is here.")
